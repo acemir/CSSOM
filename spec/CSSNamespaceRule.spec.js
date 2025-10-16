5a863c0300297048376c7254b037391e6bdb564e
Acemir Sousa Mendes <acemirsm@gmail.com>
2025-10-16 13:43:58 +0100

    fix: support namespace with filled unquoted url (#31)
    * fix: support namespace with filled unquoted url
    
    * chore: add changeset

--- spec/CSSNamespaceRule.spec.js
+++ spec/CSSNamespaceRule.spec.js
@@ -21,5 +21,21 @@ describe('CSSOM', function() {
             expect(rule.namespaceURI).toBe('');
             expect(rule.cssText).toBe('@namespace url();');
         });
+
+        given('@namespace url(http://example.com);', function(cssText) {
+            var rule = new CSSOM.CSSNamespaceRule;
+            rule.cssText = cssText;
+            expect(rule.prefix).toBe('');
+            expect(rule.namespaceURI).toBe('http://example.com');
+            expect(rule.cssText).toBe('@namespace url(http://example.com);');
+        });
+
+        given('@namespace a url(http://example.com);', function(cssText) {
+            var rule = new CSSOM.CSSNamespaceRule;
+            rule.cssText = cssText;
+            expect(rule.prefix).toBe('a');
+            expect(rule.namespaceURI).toBe('http://example.com');
+            expect(rule.cssText).toBe('@namespace a url(http://example.com);');
+        });
     });
 });
