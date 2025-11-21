a6113ebe1dfda3ed098746cf49d91438dfd8ed32
Acemir Sousa Mendes <acemirsm@gmail.com>
2025-11-21 15:13:10 +0000

    fix: serialization with empty nested declarations

--- spec/CSSStyleRule.spec.js
+++ spec/CSSStyleRule.spec.js
@@ -13,5 +13,28 @@ describe('CSSStyleRule', function() {
 		expect(rule.cssText).toBe('h1.title { font-size: 3em; }');
 	});
 
+	describe('Serialzation', function() {
+		it('Empty CSSNestedDeclarations', function() {
+			var s = new CSSOM.CSSStyleSheet;
+			s.insertRule('.a { & { } left: 1px; & { } right: 1px; }');
+			var rule = s.cssRules[0];
+			expect(rule.cssText).toBe('.a {\n  & { }\n  left: 1px;\n  & { }\n  right: 1px;\n}');
+			for (var i = 0; i < rule.cssRules.length; i++) {
+				rule.cssRules[i].style = '';
+			}
+			expect(rule.cssText).toBe('.a {\n  & { }\n  & { }\n}');
+		});
+		it('Empty CSSNestedDeclarations (nested grouping rule)', function() {
+			var s = new CSSOM.CSSStyleSheet;
+			s.insertRule('.a {\n  @media (width > 1px) {\n  & { }\n  left: 1px;\n  & { }\n  right: 1px;\n}\n}');
+			var rule = s.cssRules[0];
+			var mediaRule = s.cssRules[0].cssRules[0];
+			expect(rule.cssText).toBe('.a {\n  @media (width > 1px) {\n  & { }\n  left: 1px;\n  & { }\n  right: 1px;\n}\n}');
+			for (var i = 0; i < mediaRule.cssRules.length; i++) {
+				mediaRule.cssRules[i].style = '';
+			}
+			expect(rule.cssText).toBe('.a {\n  @media (width > 1px) {\n  & { }\n  & { }\n}\n}');
+		});
+	});
 });
 });
