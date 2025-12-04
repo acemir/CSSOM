31b522ce0a6237d87da74a491a33283d242399ff
Acemir Sousa Mendes <acemirsm@gmail.com>
2025-12-04 15:21:33 +0000

    fix: CSSImportRule tests

--- spec/CSSImportRule.spec.js
+++ spec/CSSImportRule.spec.js
@@ -2,54 +2,68 @@ describe('CSSOM', function() {
 describe('CSSImportRule', function() {
 
 	given('@import url(button.css);', function(cssText) {
-		var rule = new CSSOM.CSSImportRule;
-		rule.cssText = cssText;
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
 		expect(rule.href).toBe('button.css');
 		expect([].join.call(rule.media, ', ')).toBe('');
 	});
 
 	given('@import url("button.css");', function(cssText) {
-		var rule = new CSSOM.CSSImportRule;
-		rule.cssText = cssText;
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
 		expect(rule.href).toBe('button.css');
 		expect([].join.call(rule.media, ', ')).toBe('');
 	});
 
 	given("@import url('button.css');", function(cssText) {
-		var rule = new CSSOM.CSSImportRule;
-		rule.cssText = cssText;
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
 		expect(rule.href).toBe('button.css');
 		expect([].join.call(rule.media, ', ')).toBe('');
 	});
 
 	given('@import "button.css";', function(cssText) {
-		var rule = new CSSOM.CSSImportRule;
-		rule.cssText = cssText;
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
 		expect(rule.href).toBe('button.css');
 		expect([].join.call(rule.media, ', ')).toBe('');
 	});
 
 	given("@import 'button.css';", function(cssText) {
-		var rule = new CSSOM.CSSImportRule;
-		rule.cssText = cssText;
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
 		expect(rule.href).toBe('button.css');
 		expect([].join.call(rule.media, ', ')).toBe('');
 	});
 
 	given('@import url(size/medium.css) all;', function(cssText) {
-		var rule = new CSSOM.CSSImportRule;
-		rule.cssText = '@import url(size/medium.css) all;';
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
 		expect(rule.href).toBe('size/medium.css');
 		expect([].join.call(rule.media, ', ')).toBe("all");
 		expect(rule.media.mediaText).toBe("all");
 	});
 
 	given('@import url(old.css) screen and (color), projection and (min-color: 256);', function(cssText) {
-		var rule = new CSSOM.CSSImportRule;
-		rule.cssText = '@import url(old.css) screen and (color), projection and (min-color: 256);';
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
 		expect(rule.href).toBe('old.css');
 		expect([].join.call(rule.media, ', ')).toBe('screen and (color), projection and (min-color: 256)');
 		expect(rule.media.mediaText).toBe('screen and (color), projection and (min-color: 256)');
+	});
+
+	given('@import "http:::bar";', function(cssText) {
+		var sheet = CSSOM.parse(cssText);
+		expect(sheet.cssRules.length).toBe(1);
+		var rule = sheet.cssRules[0];
+		expect(rule.href).toBe('http:::bar');
 	});
 });
 });
